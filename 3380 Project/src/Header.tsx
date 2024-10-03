6b53c30d653de108c7d0d2c62e706e2c114307a0
myenne1 <yennesmuhanad@gmail.com>
2024-10-03 16:16:22 -0500

    removed unnecessary imports

--- 3380 Project/src/Header.tsx
+++ 3380 Project/src/Header.tsx
@@ -1,11 +1,4 @@
 import React from "react";
-import {
-  BsFillBellFill,
-  BsFillEnvelopeFill,
-  BsPersonCircle,
-  BsSearch,
-  BsJustify,
-} from "react-icons/bs";
 
 function Header() {
   return <header className="header"></header>;
