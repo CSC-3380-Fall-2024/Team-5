90a734f2db074e9e4addefc0851f590ff421a063
myenne1 <yennesmuhanad@gmail.com>
2024-10-13 20:50:38 -0500

    Website now updates page content

--- 3380 Project/src/App.tsx
+++ 3380 Project/src/App.tsx
@@ -1,13 +1,28 @@
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CategoryCreation from "./pages/category_creation";
+import RemoteSelection from "./pages/remoteSelection";
+import UpdateNotes from "./pages/updateNotes";
+import TaskMap from "./pages/taskMap";
 
 function App() {
   return (
-    <div className="grid-container">
-      <Header />
-      <Sidebar />
-    </div>
+    <>
+      <Router>
+        <div className="grid-container">
+          <Header />
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<CategoryCreation />} />
+            <Route path="remoteSelection" element={<RemoteSelection />} />
+            <Route path="updateNotes" element={<UpdateNotes />} />
+            <Route path="taskMap" element={<TaskMap />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ 3380 Project/src/pages/category_creation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function category_creation() {
+  return (
+    <div>
+      Category Creation
+    </div>
+  )
+}
+
+export default category_creation
