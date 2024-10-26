b0d96ff6810625bcea5eb5be621cbd6236634335
Tien Vo <tvo34@lsu.edu>
2024-10-25 22:18:40 -0500

    front end of subject tabs completed

--- 3380 Project/src/App.tsx
+++ 3380 Project/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
@@ -6,24 +7,26 @@ import CategoryCreation from "./pages/category_creation";
 import RemoteSelection from "./pages/remoteSelection";
 import UpdateNotes from "./pages/updateNotes";
 import TaskMap from "./pages/taskMap";
+import SubjectPage from "./SubjectPage";
+import SubjectTabs from "./SubjectTabs";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <Router>
-        <div className="grid-container">
-          <Header />
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<CategoryCreation />} />
-            <Route path="remoteSelection" element={<RemoteSelection />} />
-            <Route path="updateNotes" element={<UpdateNotes />} />
-            <Route path="taskMap" element={<TaskMap />} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <div className="grid-container">
+        <Header />
+        <Sidebar />
+        <SubjectTabs />
+        <Routes>
+          <Route path="/" element={<CategoryCreation />} />
+          <Route path="remoteSelection" element={<RemoteSelection />} />
+          <Route path="updateNotes" element={<UpdateNotes />} />
+          <Route path="taskMap" element={<TaskMap />} />
+          <Route path="subject/:subjectName" element={<SubjectPage />} />{" "}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
