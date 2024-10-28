63bb15cfcbadbc92f597e2053120355a38aea418
NathanPham <npham45@gmail.com>
2024-10-28 16:01:16 -0500

    fix bug on TabCreation and remoteSelection

--- 3380 Project/src/pages/remoteSelection.tsx
+++ 3380 Project/src/pages/remoteSelection.tsx
@@ -1,55 +1,50 @@
-import React, {useRef, useState, } from "react";
+import { useState } from "react";
 import "../CSS Files/remoteSelection.css";
 import "../CSS Files/CategoryComponent.css";
-import "../CSS Files/App.css"
+import "../CSS Files/App.css";
 import Tabs from "../components/TabCreation.tsx";
-import portrait from "../assets/Default_pfp.svg.png"
+//import portrait from "../assets/Default_pfp.svg.png";
 import { FcCheckmark } from "react-icons/fc";
-import CheckIcon from '@mui/icons-material/Check';
-
+import { IoCheckmarkOutline } from "react-icons/io5";
 
 function remoteSelection() {
- const dummyTask = ["swag"]; //placeholder
- //const dummyUser = ["lebron"]//placeholder
+  const dummyTask = ["swag"]; //placeholder
+  //const dummyUser = ["lebron"]//placeholder
   //Updates array that holds user ID and tasks assigned to them
 
-const  [isActive, setIsActive] = useState(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isActive, setIsActive] = useState(false);
 
-
-const handleClick = () => {
-  setIsActive(!isActive);
-    
-  }
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
 
   return (
     <div className="main-container">
       <div className="categories-wrapper">
-          <Tabs>
-             <div label="Tasks">
-       <div className="task-container">
-        {dummyTask.length === 0 ? (<p  className="no-categories">No Tasks</p>) : (dummyTask)
-        } 
-        
-        <button
-          className="task-button" 
-          onClick={handleClick}
-          
-          >
-           {isActive ? (<FcCheckmark className="task-icon"/>) : (<CheckIcon className="second-icon"/>)}
-          </button>
-            test
-          </div>
-
-          </div>
-          <div label="Users">
-             <img src = {portrait} />
+        <Tabs>
+          <div className="Tasks">
+            <div className="task-container">
+              {dummyTask.length === 0 ? (
+                <p className="no-categories">No Tasks</p>
+              ) : (
+                dummyTask
+              )}
+              <button className="task-button" onClick={handleClick}>
+                {isActive ? (
+                  <FcCheckmark className="task-icon" />
+                ) : (
+                  <IoCheckmarkOutline className="second-icon" />
+                )}
+              </button>
               test
+            </div>
           </div>
-      </Tabs>
+          <div className="Users">test</div>
+        </Tabs>
       </div>
-   </div>
-  )
+    </div>
+  );
 }
 
-export default remoteSelection
-
+export default remoteSelection;
