494406114a77f1794d3303d40fe2ea4c4c131947
NathanPham <npham45@gmail.com>
2024-11-07 10:19:32 -0600

    fix currentUser is null when it is already existed

--- 3380 Project/src/authContext.jsx
+++ 3380 Project/src/authContext.jsx
@@ -19,13 +19,10 @@ export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState({});
 
   function signup(email, password) {
-    console.log(auth.currentUser);
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
   function login(email, password) {
-    console.log(auth.currentUser);
-    console.log("Email", email);
     return signInWithEmailAndPassword(auth, email, password);
   }
 
@@ -38,6 +35,14 @@ export function AuthProvider({ children }) {
     return signOut(auth);
   }
 
+  async function getContacts() {
+    await authStateReady(auth);
+    currentUser = auth.currentUser;
+
+    if (!currentUser) return false;
+    console.log(currentUser);
+    return currentUser;
+  }
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log("Auth", user);
@@ -53,6 +58,7 @@ export function AuthProvider({ children }) {
     googleSignIn,
     signup,
     logout,
+    getContacts,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
