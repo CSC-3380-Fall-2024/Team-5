741077307f6e033bba66f5d1ade051f70af9090c
NathanPham <npham45@gmail.com>
2024-11-07 00:33:38 -0600

    username/email and password are saved in firestore

---
+++ 3380 Project/src/authContext.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useState, useEffect } from "react";
+import { auth, database } from "./fireBase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState({});
+
+  function signup(email, password) {
+    console.log(auth.currentUser);
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(email, password) {
+    console.log(auth.currentUser);
+    console.log("Email", email);
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function googleSignIn() {
+    const googleAuthProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleAuthProvider);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("Auth", user);
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    googleSignIn,
+    signup,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
--- 3380 Project/src/fireBase 2.js
+++ 3380 Project/src/fireBase 2.js
@@ -1,21 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKibGVw_uoTs9ewxJsbL7hQBCGKX83EBo",
-  authDomain: "demologin-5ef72.firebaseapp.com",
-  projectId: "demologin-5ef72",
-  storageBucket: "demologin-5ef72.appspot.com",
-  messagingSenderId: "865731909896",
-  appId: "1:865731909896:web:2e3c873744b015a733b4bd",
-  measurementId: "G-HH6VKNTECS",
+  apiKey: "AIzaSyAIEdb5s-OadAMVbagMRjLpgYuO7Gy2Tx4",
+  authDomain: "project-management-team5.firebaseapp.com",
+  projectId: "project-management-team5",
+  storageBucket: "project-management-team5.firebasestorage.app",
+  messagingSenderId: "142493769762",
+  appId: "1:142493769762:web:d4b7300f12f08a9669aa7b",
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+//Authentication
+export const auth = getAuth(app);
+export const database = getFirestore(app);
+export default app;
