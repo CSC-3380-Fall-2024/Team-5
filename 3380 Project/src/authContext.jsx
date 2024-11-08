56fc37f3d920d2aa4b17fd4db6567c8257e43767
NathanPham <npham45@gmail.com>
2024-11-08 14:39:34 -0600

    update firebase api and log out function

--- 3380 Project/src/authContext.jsx
+++ 3380 Project/src/authContext.jsx
@@ -35,14 +35,6 @@ export function AuthProvider({ children }) {
     return signOut(auth);
   }
 
-  async function getContacts() {
-    await authStateReady(auth);
-    currentUser = auth.currentUser;
-
-    if (!currentUser) return false;
-    console.log(currentUser);
-    return currentUser;
-  }
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log("Auth", user);
@@ -58,7 +50,6 @@ export function AuthProvider({ children }) {
     googleSignIn,
     signup,
     logout,
-    getContacts,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
