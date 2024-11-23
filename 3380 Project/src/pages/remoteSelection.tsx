3bc6a724e67e6bd7b5b2b1e865e14c15fa8cf30a
Jordan <jberg95@lsu.edu>
2024-11-23 14:57:02 -0600

    Implements UI and clean a CSS layout for remote selection

--- 3380 Project/src/pages/remoteSelection.tsx
+++ 3380 Project/src/pages/remoteSelection.tsx
@@ -3,45 +3,48 @@ import "../CSS Files/remoteSelection.css";
 import "../CSS Files/CategoryComponent.css";
 import "../CSS Files/App.css";
 import Tabs from "../components/TabCreation.tsx";
-//import portrait from "../assets/Default_pfp.svg.png";
+import portrait from "../assets/Default_pfp.png"
 import { FcCheckmark } from "react-icons/fc";
 import { IoCheckmarkOutline } from "react-icons/io5";
 
 function remoteSelection() {
-  const dummyTask = ["swag"]; //placeholder
-  //const dummyUser = ["lebron"]//placeholder
+ const dummyTask = ["Task 1"]; //placeholder
+ const dummyUser = ["Jordan"]//placeholder
   //Updates array that holds user ID and tasks assigned to them
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [isActive, setIsActive] = useState(false);
 
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
+
+
+const handleClick = () => {
+  setIsActive(!isActive);
+    
+  }
 
   return (
     <div className="main-container">
       <div className="categories-wrapper">
-        <Tabs>
-          <div className="Tasks">
-            <div className="task-container">
-              {dummyTask.length === 0 ? (
-                <p className="no-categories">No Tasks</p>
-              ) : (
-                dummyTask
-              )}
-              <button className="task-button" onClick={handleClick}>
-                {isActive ? (
-                  <FcCheckmark className="task-icon" />
-                ) : (
-                  <IoCheckmarkOutline className="second-icon" />
-                )}
-              </button>
-              test
-            </div>
+
+       <div className="task-container">
+        {dummyTask.length === 0 ? (<p  className="no-categories">No Tasks</p>) : (dummyTask)
+        } 
+          {isActive &&  (<p className="user">{dummyUser}</p>)  }
+         
+         
+         
+        <button
+          className="task-button" 
+          onClick={handleClick}
+          
+          >
+           {isActive ? (<FcCheckmark className="task-icon"/>) : (<CheckIcon className="second-icon"/>)}
+          </button>
+            
           </div>
-          <div className="Users">test</div>
-        </Tabs>
+
+          
+      
       </div>
     </div>
   );
