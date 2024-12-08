46633fa6b4662d36fc0de43fe9498aeb53a19dd7
Jordan <jberg95@lsu.edu>
2024-12-07 18:36:44 -0600

    Implemented backend support yahoo!

--- 3380 Project/src/pages/remoteSelection.tsx
+++ 3380 Project/src/pages/remoteSelection.tsx
@@ -1,45 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../CSS Files/remoteSelection.css";
 import "../CSS Files/CategoryComponent.css";
 import "../CSS Files/App.css";
-import Tabs from "../components/TabCreation.tsx";
-import portrait from "../assets/Default_pfp.png";
 import { FcCheckmark } from "react-icons/fc";
 import { IoCheckmarkOutline } from "react-icons/io5";
-import { database } from "../fireBase 2.js";
+import { fetchTasks } from "../firebase/CategoryCreationCrudFunctions.js";
+
+const teamId = "B18T0M2TwLngVuq8opN1";
+
+//Interface solely to store data into useState array later
+interface Tasks {
+  id: string;
+  description: string;
+}
 function remoteSelection() {
-  const dummyTask = ["Task 1"]; //placeholder
-  const dummyUser = ["Jordan"]; //placeholder
-  const taskRef = database.ref('tasks');
-  taskRef.on('child_added', function(snapshot) {
-    snapshot.forEach(function(childSnapshot){
-       const childData = childSnapshot.val();
-    });
-  });
-  const categoryDesignation = taskRef.categoryId;
 
-  if (categoryDesignation == 'to-do'){
-    //have it render
-  }
-
+  const [task, setTask] = useState<Tasks[]>([]);
   const [isActive, setIsActive] = useState(false);
+  //loads tasks
+  const loadTasks = async () => {
+    try {
+      const fetchedTasks = await fetchTasks("to-do", teamId);
+      const updatedTasks = fetchedTasks.map((tasks) => ({
+        id: tasks.id,
+        description: tasks.description,
+      }));
+      setTask(updatedTasks); //LOAD THE TASKS I BEG OF YOU
+    } catch (error) {
+      console.error("error loading tasks: ", error);
+    }
+  };
+  //once a change has been made, load tasks from to-do
+  useEffect(() => {
+    loadTasks();
+    console.log("loaded tasks")
+  }, []);
+  
+  
+
+  //handles click op
 
   const handleClick = () => {
     setIsActive(!isActive);
   };
-
+ 
   return (
     <div className="main-container">
       <div className="categories-wrapper">
         <div className="task-container">
-          {dummyTask.length === 0 ? (
+          {task.length === 0 ? (
             <p className="no-categories">No Tasks</p>
           ) : (
-            dummyTask
+           task.map((tasks) => tasks.description)
           )}
-          {isActive && <p className="user">{dummyUser}</p>}
+          {isActive && <p className="user">{"Jordan"}</p>}
 
-          <button className="task-button" onClick={handleClick}>
+          <button className="task-button"  onClick={() => handleClick}>
             {isActive ? (
               <FcCheckmark className="task-icon" />
             ) : (
