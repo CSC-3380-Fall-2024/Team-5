68cad2b2f1370ce1f54eda8f79df141181789a0b
Hope McMillen <hmcmil5@lsu.edu>
2024-12-07 21:42:04 -0600

    changes to the CSS files for the sidebar and the category creation that allow a new format to the colors as well as ading a collapsable/expandable feature to the sidebar

--- 3380 Project/src/components/Sidebar.tsx
+++ 3380 Project/src/components/Sidebar.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoCreateOutline } from "react-icons/io5";
 import { IoIosCloud } from "react-icons/io";
 import { FaRegNoteSticky } from "react-icons/fa6";
 import { GrTree } from "react-icons/gr";
 import { Link } from "react-router-dom";
-
-
+import { IoArrowForwardCircle, IoArrowBackCircle } from "react-icons/io5";
 
 function Sidebar() {
+  const [isCollapsed, setIsCollaped] = useState(false);
+  const toggleSidebar = () => {
+    setIsCollaped(!isCollapsed);
+  }
   return (
-    <aside id="sidebar">
+    <aside id="sidebar" className={isCollapsed? "collapsed" : "expanded"}>
       <div className="sidebar-title">
         <div className="sidebar-brand">
           <h1 className="sidebar-header">Task Management</h1>
@@ -17,25 +20,33 @@ function Sidebar() {
         <span className="icon close_icon">X</span>
       </div>
 
+      <button className="sidebar-toggle" onClick={toggleSidebar}>
+        {isCollapsed ? (
+          <IoArrowForwardCircle className="toggle-icon"/>
+        ) : (
+          <IoArrowBackCircle className="toggle-icon"/>
+        )}
+      </button>
+
       <ul className="sidebar-list">
         <li className="sidebar-list-item">
-          <Link to="/categoryCreation">
-            <IoCreateOutline className="icon" /> Category Creation
+          <Link to="/">
+            <IoCreateOutline className="icon" /> {isCollapsed ? "" : "Category Creation"}
           </Link>
         </li>
         <li className="sidebar-list-item">
           <Link to="/remoteSelection">
-            <IoIosCloud className="icon" /> Remote Selection
+            <IoIosCloud className="icon" /> {isCollapsed ? "" : "Remote Selection"}
           </Link>
         </li>
         <li className="sidebar-list-item">
           <Link to="/updateNotes">
-            <FaRegNoteSticky className="icon" /> Update Notes
+            <FaRegNoteSticky className="icon" /> {isCollapsed ? "" : "Update Notes"}
           </Link>
         </li>
         <li className="sidebar-list-item">
           <Link to="/taskMap">
-            <GrTree className="icon" /> Task Map
+            <GrTree className="icon" /> {isCollapsed ? "" : "Task Map"}
           </Link>
         </li>
       </ul>
@@ -44,3 +55,4 @@ function Sidebar() {
 }
 
 export default Sidebar;
+
