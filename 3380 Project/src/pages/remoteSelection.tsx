9b448ddcaf3239f9c3044efad830a254d061a294
Jordan <jberg95@lsu.edu>
2024-12-10 16:48:01 -0600

    Implemented 99% of backend support for remote remoteSelection including a component, crud file, and more to the original page

--- 3380 Project/src/pages/remoteSelection.tsx
+++ 3380 Project/src/pages/remoteSelection.tsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
+import RemoteSelectionComponent from "../components/RemoteSelectionComponent.tsx"
 import "../CSS Files/remoteSelection.css";
 import "../CSS Files/CategoryComponent.css";
 import "../CSS Files/App.css";
-import { FcCheckmark } from "react-icons/fc";
-import { IoCheckmarkOutline } from "react-icons/io5";
-import { fetchTasks } from "../firebase/CategoryCreationCrudFunctions.js";
+
+import { fetchTasks } from "../firebase/CategoryCreationCrudFunctions.ts";
 
 const teamId = "B18T0M2TwLngVuq8opN1";
 
 //Interface solely to store data into useState array later
-interface Tasks {
+export interface Tasks {
   id: string;
   description: string;
+  element: JSX.Element;
 }
+
+
 function remoteSelection() {
 
   const [task, setTask] = useState<Tasks[]>([]);
-  const [isActive, setIsActive] = useState(false);
   //loads tasks
   const loadTasks = async () => {
     try {
@@ -24,8 +26,15 @@ function remoteSelection() {
       const updatedTasks = fetchedTasks.map((tasks) => ({
         id: tasks.id,
         description: tasks.description,
+        element: (
+        <RemoteSelectionComponent
+          id={tasks.id}
+          description={tasks.description}
+        />
+        ),
       }));
       setTask(updatedTasks); //LOAD THE TASKS I BEG OF YOU
+      
     } catch (error) {
       console.error("error loading tasks: ", error);
     }
@@ -38,31 +47,12 @@ function remoteSelection() {
   
   
 
-  //handles click op
 
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
  
   return (
     <div className="main-container">
-      <div className="categories-wrapper">
-        <div className="task-container">
-          {task.length === 0 ? (
-            <p className="no-categories">No Tasks</p>
-          ) : (
-           task.map((tasks) => tasks.description)
-          )}
-          {isActive && <p className="user">{"Jordan"}</p>}
-
-          <button className="task-button"  onClick={() => handleClick}>
-            {isActive ? (
-              <FcCheckmark className="task-icon" />
-            ) : (
-              <IoCheckmarkOutline className="second-icon" />
-            )}
-          </button>
-        </div>
+        <div className="categories-wrapper">
+         {task.map((tasks) => tasks.element)}
       </div>
     </div>
   );
